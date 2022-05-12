f6e20c353e3626906031ed12aff9261b433c3e34
Chase Lonas <chaselonas@gmail.com>
2022-05-12 06:45:53 -0600

    added dirty data set for cleaning practice

--- pandaPrac.py
+++ pandaPrac.py
@@ -1,10 +1,4 @@
 import pandas as pd
 
-data = {
-    
-    "calories": [420, 380, 390],
-    "duration": [50, 40, 45]
-}
-myvar = pd.DataFrame(data)
+df = pd.read_csv('dirtydata.csv')
 
-print(myvar)
